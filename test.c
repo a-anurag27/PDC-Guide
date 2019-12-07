c84ffeb0b29bed84d382137330ebf11113975af8
Anurag A <anurag2798@gmail.com>
2019-12-07 12:32:07 +0530

    Test

---
+++ test.c
@@ -0,0 +1,31 @@
+//**Environment Variable**
+//export OMP_NUM_THREADS=20
+
+//gcc test.h -fopenmp
+//./a.out
+
+
+#include<stdio.h>
+
+//Header File
+#include<omp.h>
+
+void main()
+{
+
+	int tid,nt;
+
+	//Set threads through runtime
+	omp_set_num_threads(10);
+	
+	//Directive to Start OMP
+	#pragma omp parallel
+	
+	{
+		//Some inbuilt functions
+		tid = omp_get_thread_num();
+		nt = omp_get_num_threads();
+		printf("Hello World %d / %d\n",tid,nt);
+	}
+
+}
